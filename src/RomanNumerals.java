fe0f5817987ea35d19ed3195fc27f5db9d054582
havivha <haviv2001@walla.com>
2012-12-09 21:58:51 -0500

    Green

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,7 +4,29 @@ public class RomanNumerals
 	public String toRoman(int number) 
 	{
 		String RomanValue = "";
-		
+		while (number > 9)
+		{
+			RomanValue = RomanValue + "X";
+			number = number - 10;
+		}
+
+		if (number > 8)
+		{
+			RomanValue = RomanValue + "IX";
+			number = number - 9;
+		}
+
+		if (number > 4)
+		{
+			RomanValue = RomanValue + "V";
+			number = number - 5;
+		}
+
+		if (number > 3) 
+		{
+			RomanValue = RomanValue + "IV";
+			number = number - 4;
+		}
 		
 		while (number > 0)
 		{
